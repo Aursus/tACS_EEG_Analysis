45bd011f1c88a5a89be3b9999c37f5d1cb6c8e01
Aursus <97478920+Aursus@users.noreply.github.com>
2025-01-29 21:39:57 -0500

    Delete New folder directory

--- New folder/ica_test.m
+++
@@ -1,7 +0,0 @@
-%% Test Data For ICA
-EEG_before = pop_loadset('filename', '3014-2 ICA.set');
-EEG_after= pop_loadset('filename', '3014-2 ICA 2.set');
-subplot(211)
-plot (EEG_before.times', EEG_before.data(:,:,1)');
-subplot(212)
-plot (EEG_after.times', EEG_after.data(:,:,1)');
